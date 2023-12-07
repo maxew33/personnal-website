78b802269e5eab10e8c12d8af3edf465fd6237ad
Maxime Malfilâtre <maxime.malfilatre@gmail.com>
2023-12-07 16:10:40 +0100

    :lipstick: Change header nav height, improve hero

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,23 +3,43 @@ import Header from '@/components/header/Header'
 import Head from 'next/head'
 import Link from 'next/link'
 import styles from './Home.module.css'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+    const [imageData, setImageData] = useState('')
+
+    useEffect(() => {
+        setImageData(
+            '/assets/portraits/quinten-de-graaf-iwBfqKwaJQI-unsplash.jpg'
+        )
+    }, [])
+
     return (
         <>
             <main>
                 <section className={styles.hero}>
                     <span className={styles.txtHook1}>
-                        Soyez {' '}
+                        Soyez{' '}
                         <span className={styles.highlighted}>En ligne</span>
                     </span>
                     <span className={styles.txtHook2}>
-                        Avec un site <br/>qui{' '}
-                        <span className={styles.highlighted}>vous</span>{' '}
+                        Avec un site <br />
+                        qui <span className={styles.highlighted}>
+                            vous
+                        </span>{' '}
                         ressemble.
                     </span>
                     <div className={styles.slider}>
-                        <div className={styles.imgContainer}></div>
+                        <div className={styles.imgContainer}>
+                            <Image
+                                src={imageData}
+                                alt="eyes"
+                                layout="fill"
+                                objectFit="cover"
+                                // className={styles.imgContainer}
+                            />
+                        </div>
                     </div>
                 </section>
             </main>
