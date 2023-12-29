9e94f4034aab6f51e986e16be8986f883aefdb48
Maxime Malfilâtre <maxime.malfilatre@gmail.com>
2023-12-29 15:02:27 +0100

    :lipstick: Contact - v1 done

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -8,9 +8,11 @@ import {
     faSquareFacebook,
 } from '@fortawesome/free-brands-svg-icons'
 import {
+    faCalendarDays,
     faEnvelope,
     faMobileScreen,
     faPaperPlane,
+    faThumbsUp,
 } from '@fortawesome/free-solid-svg-icons'
 
 export default function Contact() {
@@ -21,126 +23,157 @@ export default function Contact() {
     }
 
     return (
+        <>
+        <header className={styles.pageTitle}>Nous contacter</header>
         <main className={styles.main}>
-            {/* <Image
-                src="/assets/contact/phone.png"
+        {/* <Image
+            src="/assets/contact/phone.png"
+            height={600}
+            width={400}
+            className={styles.image}
+            alt="nous contacter Velvet Studio"
+        /> */}
+
+        <section className={styles.title}>
+            <article>
+                <h3>Ensemble, explorons les opportunités qui s'offrent à vous.</h3>
+                {/* <Image
+                src="/assets/contact/phone.jpg"
                 height={600}
                 width={400}
                 className={styles.image}
                 alt="nous contacter Velvet Studio"
             /> */}
+            </article>
+        </section>
 
-            <section>
-                <Image
-                    src="/assets/contact/phone.jpg"
-                    height={600}
-                    width={400}
-                    className={styles.image}
-                    alt="nous contacter Velvet Studio"
-                />
-            </section>
-
-            <section className={styles.contactUs}>
-                <article>
-                    <h2>Une question ?</h2>
-                    <p className={styles.prez}>
-                        Envoyez-nous un message. <br />
-                        Nous vous répondrons dès que possible.
-                    </p>
-                    <form
-                        ref={form}
-                        onSubmit={sendEmail}
-                        className={styles.formWrapper}
-                    >
-                        <div className={styles.inputWrapper}>
-                            <input
-                                id="nameInput"
-                                type="text"
-                                name="from_name"
-                                className={styles.input}
-                                required
-                            />
-                            <label htmlFor="nameInput" className={styles.label}>
-                                Nom
-                            </label>
-                        </div>
-                        <div className={styles.inputWrapper}>
-                            <input
-                                id="emailInput"
-                                type="email"
-                                name="user_email"
-                                className={styles.input}
-                                required
-                            />
-                            <label
-                                htmlFor="emailInput"
-                                className={styles.label}
-                            >
-                                Email
-                            </label>
-                        </div>
-                        <div className={styles.messageWrapper}>
-                            <textarea
-                                id="messageInput"
-                                name="message"
-                                className={styles.textArea}
-                                rows="5"
-                                required
-                                defaultValue={''}
-                            />
-                            <label
-                                htmlFor="messageInput"
-                                className={styles.label}
-                            >
-                                Message
-                            </label>
-                        </div>
-                        <button type="submit" className={styles.button}>
-                            envoyer
-                            <FontAwesomeIcon icon={faPaperPlane} />
-                        </button>
-                    </form>
-                </article>
-            </section>
-            
-            <section className={styles.linksWrapper}>
-                <article>
-                    {/* <!-- Google Calendar Appointment Scheduling begin --> */}
-                    <h2>rencontrez-nous</h2>
-                    <a href="https://calendar.app.google/x5FM2reA2PDo4JkA8">
-                        prendre rendez-vous
-                    </a>
-                    {/* <!-- end Google Calendar Appointment Scheduling --> */}
-                </article>
-                <article>
-                    <h2>Contactez-nous</h2>
-                    <a href="tel:07555555" className={styles.contact}>
-                        <FontAwesomeIcon
-                            icon={faMobileScreen}
-                            className={styles.icon}
+        <section className={styles.contactUs}>
+            <article>
+                <h2>Une question ?</h2>
+                <p className={styles.prez}>
+                    Envoyez-nous un message. <br />
+                    Nous vous répondrons dès que possible.
+                </p>
+                <form
+                    ref={form}
+                    onSubmit={sendEmail}
+                    className={styles.formWrapper}
+                >
+                    <div className={styles.inputWrapper}>
+                        <input
+                            id="nameInput"
+                            type="text"
+                            name="from_name"
+                            className={styles.input}
+                            required
                         />
+                        <label htmlFor="nameInput" className={styles.label}>
+                            Nom
+                        </label>
+                    </div>
+                    <div className={styles.inputWrapper}>
+                        <input
+                            id="emailInput"
+                            type="email"
+                            name="user_email"
+                            className={styles.input}
+                            required
+                        />
+                        <label
+                            htmlFor="emailInput"
+                            className={styles.label}
+                        >
+                            Email
+                        </label>
+                    </div>
+                    <div className={styles.messageWrapper}>
+                        <textarea
+                            id="messageInput"
+                            name="message"
+                            className={styles.textArea}
+                            rows="5"
+                            required
+                            defaultValue={''}
+                        />
+                        <label
+                            htmlFor="messageInput"
+                            className={styles.label}
+                        >
+                            Message
+                        </label>
+                    </div>
+                    <button type="submit" className={styles.button}>
+                        envoyer
+                        <FontAwesomeIcon icon={faPaperPlane} />
+                    </button>
+                </form>
+            </article>
+        </section>
+
+        <section className={styles.linksWrapper}>
+            <article>
+                {/* <!-- Google Calendar Appointment Scheduling begin --> */}
+                <h2>
+                    <FontAwesomeIcon
+                        icon={faCalendarDays}
+                        className={styles.icon}
+                    />{' '}
+                    Rencontrez-nous
+                </h2>
+                <div className={styles.contentWrapper}>
+                    <a href="https://calendar.app.google/x5FM2reA2PDo4JkA8">
+                        Prendre rendez-vous
+                    </a>
+                </div>
+                {/* <!-- end Google Calendar Appointment Scheduling --> */}
+            </article>
+            <article>
+                <h2>
+                    <FontAwesomeIcon
+                        icon={faMobileScreen}
+                        className={styles.icon}
+                    />{' '}
+                    Appelez-nous
+                </h2>
+                <div className={styles.contentWrapper}>
+                    <a href="tel:07555555" className={styles.contact}>
                         07-555-555
                     </a>
+                </div>
+            </article>
+            <article>
+                <h2>
+                    <FontAwesomeIcon
+                        icon={faEnvelope}
+                        className={styles.icon}
+                    />{' '}
+                    écrivez-nous
+                </h2>
+                <div className={styles.contentWrapper}>
                     <a
                         href="mailto:contact@velvetweb.fr"
                         className={styles.contact}
                     >
-                        <FontAwesomeIcon
-                            icon={faEnvelope}
-                            className={styles.icon}
-                        />
                         contact@velvetweb.fr
                     </a>
-                </article>
-                <article className={styles.followUs}>
-                    <h2>Suivez-nous</h2>
+                </div>
+            </article>
+            <article className={styles.followUs}>
+                <h2>
+                    <FontAwesomeIcon
+                        icon={faThumbsUp}
+                        className={styles.icon}
+                    />{' '}
+                    Suivez-nous
+                </h2>
+                <div className={styles.contentWrapper}>
                     <a
                         href="https://www.linkedin.com/in/maxime-malfilatre/"
                         className={styles.contact}
                     >
                         <FontAwesomeIcon
                             icon={faLinkedin}
-                            className={styles.icon}
+                            className={styles.socialIcon}
                         />
                     </a>
                     <a
@@ -149,11 +182,14 @@ export default function Contact() {
                     >
                         <FontAwesomeIcon
                             icon={faSquareFacebook}
-                            className={styles.icon}
+                            className={styles.socialIcon}
                         />
                     </a>
-                </article>
-            </section>
-        </main>
+                </div>
+            </article>
+        </section>
+    </main>
+        </>
+        
     )
 }
