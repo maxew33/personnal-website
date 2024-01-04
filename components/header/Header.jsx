1b8708bed5b4da09501326cdeb0d979d47719109
Maxime Malfilâtre <maxime.malfilatre@gmail.com>
2024-01-04 16:04:33 +0100

    :iphone: [Header] burger nav done

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,34 +1,43 @@
 'use client'
- 
+
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Header.module.css'
 
 export default function Header() {
+    const [menuOpen, setMenuOpen] = useState(false)
     const pathname = usePathname()
 
-    const paths =[
+    const paths = [
         {
-            'path':'/',
-            'name':'accueil'
+            path: '/',
+            name: 'accueil',
         },
         {
-            'path':'/projects/',
-            'name':'projets'
+            path: '/projects/',
+            name: 'projets',
         },
         {
-            'path':'/pricing/',
-            'name':'tarifs'
+            path: '/pricing/',
+            name: 'tarifs',
         },
     ]
 
-    console.log(pathname)
+    const displayNav = () => {
+        setMenuOpen(!menuOpen)
+    }
+
+    const hideNav = () => {
+        setMenuOpen(false)
+    }
 
     return (
         <div className={styles.headerContainer}>
-            <header className={styles.header}>
+            <header className={`${styles.header} ${
+                            menuOpen && styles.menuOpen
+                        }`}>
                 <Link href="/" className={styles.logoLink}>
                     <Image
                         src="/assets/logo.png"
@@ -38,15 +47,33 @@ export default function Header() {
                         className={styles.logo}
                     />
                 </Link>
+                <button onClick={displayNav} className={styles.burger}>
+                    <span></span>
+                </button>
                 <nav className={styles.navigation}>
                     {paths.map((path, index) => (
-                        <Link href={path.path} key={`path${index}`} className={`${styles.link} ${pathname === path.path && styles.active}`}>{path.name}</Link>
+                        <Link
+                            href={path.path}
+                            key={`path${index}`}
+                            className={`${styles.link} ${
+                                pathname === path.path && styles.active
+                            }`}
+                            onClick={hideNav} 
+                        >
+                            {path.name}
+                        </Link>
                     ))}
-                    <Link href='/contact/' className={`${styles.contact} ${pathname === '/contact/' && styles.active}`}>contact</Link>
+                    <Link
+                        href="/contact/"
+                        className={`${styles.contact} ${
+                            pathname === '/contact/' && styles.active
+                        }`}
+                        onClick={hideNav} 
+                    >
+                        contact
+                    </Link>
                 </nav>
             </header>
         </div>
     )
 }
-
-
