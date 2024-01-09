281e9d49901848b87fa997adcc1fecfc14d01b3b
Maxime Malfilâtre <maxime.malfilatre@gmail.com>
2024-01-09 12:06:33 +0100

    :dizzy: [pricing] wrapper animation added

--- pages/pricing/index.jsx
+++ pages/pricing/index.jsx
@@ -1,8 +1,39 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './Pricing.module.css'
+import { motion } from 'framer-motion'
+
+async function getPricingData() {
+    const { pricing } = await require('../../data/pricing.json')
+    return pricing
+}
 
 export default function Pricing() {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        async function fetchData() {
+            const pricesData = await getPricingData()
+            setData(pricesData)
+        }
+        fetchData()
+        console.log(data)
+    }, [])
+
+    const priceWrapperAnimationVariants = {
+        initial: (index) => ({
+            opacity: 0,
+            scale: 0,
+            x: 50 * (index - 1),
+        }),
+        animate: (index) => ({
+            opacity: 1,
+            scale: 1,
+            x: 0,
+            transition: { duration: .75, delay: 0.15 * index },
+        }),
+    }
+
     return (
         <main className={styles.main}>
             <h1 className={styles.title}>Découvrez nos offres web</h1>
@@ -10,127 +41,55 @@ export default function Pricing() {
                 Choisissez la solution adaptée à votre succès en ligne
             </h2>
             <section className={styles.pricingWrapper}>
-                <article className={`${styles.article} ${styles.basic}`}>
-                    <div className={styles.header}>
-                        à partir de <span className={styles.price}>800€</span>
-                    </div>
-                    <div className={styles.content}>
-                        <h3 className={styles.pricingTitle}>
-                            offre essentielle
-                        </h3>
-                        <h4 className={styles.pricingSubtitle}>
-                            présence en ligne
-                        </h4>
-                        <input
-                            type="checkbox"
-                            name=""
-                            id="moreBasic"
-                            className={styles.more}
-                        />
-                        <label
-                            htmlFor="moreBasic"
-                            className={styles.labelForMore}
-                        >
-                            en savoir +{' '}
-                        </label>
-                        <label
-                            htmlFor="moreBasic"
-                            className={styles.labelForLess}
-                        >
-                            fermer
-                        </label>
-                        <ul className={styles.contentList}>
-                            <li>Site vitrine</li>
-                            <li>
-                                Design basé sur un de{' '}nos modèles
-                                {/* <Link href="/templates">nos modèles</Link> */}
-                            </li>
-                            <li>Référencement de base</li>
-                        </ul>
-                        <div className={styles.footer}>
-                            <Link href="/contact">Contact</Link>
+                {data.map((price, index) => (
+                    <motion.article
+                        key={'article' + index}
+                        variants={priceWrapperAnimationVariants}
+                        initial="initial"
+                        animate="animate"
+                        custom={index}
+                        className={`${styles.article} ${styles[price.type]}`}
+                    >
+                        <div className={styles.header}>
+                            à partir de{' '}
+                            <span className={styles.price}>{price.price}</span>
                         </div>
-                    </div>
-                </article>
-                <article className={`${styles.article} ${styles.standard}`}>
-                    <div className={styles.header}>
-                        à partir de <span className={styles.price}>1500€</span>
-                    </div>
-                    <div className={styles.content}>
-                        <h3 className={styles.pricingTitle}>offre standard</h3>
-                        <h4 className={styles.pricingSubtitle}>
-                            visibilité avancée
-                        </h4>
-                        <input
-                            type="checkbox"
-                            name=""
-                            id="moreStandard"
-                            className={styles.more}
-                        />
-                        <label
-                            htmlFor="moreStandard"
-                            className={styles.labelForMore}
-                        >
-                            en savoir +{' '}
-                        </label>
-                        <label
-                            htmlFor="moreStandard"
-                            className={styles.labelForLess}
-                        >
-                            fermer
-                        </label>
-                        <ul className={styles.contentList}>
-                            <li>Site vitrine jusqu'à 5 pages</li>
-                            <li>Design personnalisé</li>
-                            <li>Formulaire de contact</li>
-                            <li>Référencement avancé</li>
-                        </ul>
-                        <div className={styles.footer}>
-                            <Link href="/contact">Contact</Link>
+                        <div className={styles.content}>
+                            <h3 className={styles.pricingTitle}>
+                                {price.title}
+                            </h3>
+                            <h4 className={styles.pricingSubtitle}>
+                                {price.subtitle}
+                            </h4>
+                            <input
+                                type="checkbox"
+                                name=""
+                                id={`more${price.type}`}
+                                className={styles.more}
+                            />
+                            <label
+                                htmlFor={`more${price.type}`}
+                                className={styles.labelForMore}
+                            >
+                                en savoir +{' '}
+                            </label>
+                            <label
+                                htmlFor={`more${price.type}`}
+                                className={styles.labelForLess}
+                            >
+                                fermer
+                            </label>
+                            <ul className={styles.contentList}>
+                                {price.features.map((feature, index) => (
+                                    <li key={index}>{feature}</li>
+                                ))}
+                            </ul>
+                            <div className={styles.footer}>
+                                <Link href="/contact">Contact</Link>
+                            </div>
                         </div>
-                    </div>
-                </article>
-                <article className={`${styles.article} ${styles.premium}`}>
-                    <div className={styles.header}>
-                        à partir de <span className={styles.price}>3000€</span>
-                    </div>
-                    <div className={styles.content}>
-                        <h3 className={styles.pricingTitle}>
-                            offre e-commerce
-                        </h3>
-                        <h4 className={styles.pricingSubtitle}>
-                            boutique en ligne
-                        </h4>
-                        <input
-                            type="checkbox"
-                            name=""
-                            id="morePremium"
-                            className={styles.more}
-                        />
-                        <label
-                            htmlFor="morePremium"
-                            className={styles.labelForMore}
-                        >
-                            en savoir +{' '}
-                        </label>
-                        <label
-                            htmlFor="morePremium"
-                            className={styles.labelForLess}
-                        >
-                            fermer
-                        </label>
-                        <ul className={styles.contentList}>
-                            <li>Site e-commerce professionnel</li>
-                            <li>Système de gestion de contenu (CMS)</li>
-                            <li>Interface utilisateur intuitive</li>
-                            <li>Formation à l'utilisation du CMS</li>
-                            <li>Référencement avancé</li>
-                        </ul>
-                        <div className={styles.footer}>
-                            <Link href="/contact">Contact</Link>
-                        </div>
-                    </div>
-                </article>
+                    </motion.article>
+                ))}
             </section>
         </main>
     )
